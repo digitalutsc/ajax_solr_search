4174ef7c570113b1c76c925e9549549f4d9cd93e
Amy Li <amyamay22@gmail.com>
2023-02-08 17:08:05 -0500

    Remove extra click event handlers from date range filter button

--- library/app/widgets/DateRangeWidget.js
+++ library/app/widgets/DateRangeWidget.js
@@ -3,27 +3,30 @@
     afterRequest: function () {
       var self = this;
 
-      $(this.target).on("click", "button", function () {
-        // get date range input
-        var start_date = $($(self.target).find("input")[0]).val();
-        var end_date = $($(self.target).find("input")[1]).val();
+      $(this.target)
+        .find("button")
+        .off()
+        .on("click", function () {
+          // get date range input
+          var start_date = $($(self.target).find("input")[0]).val();
+          var end_date = $($(self.target).find("input")[1]).val();
 
-        // if no end date given, use start date
-        if (!end_date) {
-          end_date = start_date;
-          $($(self.target).find("input")[1]).val(end_date);
-        }
+          // if no end date given, use start date
+          if (!end_date) {
+            end_date = start_date;
+            $($(self.target).find("input")[1]).val(end_date);
+          }
 
-        // query parameters
-        var min = start_date ? start_date : "*";
-        var max = end_date ? end_date : "*";
-        var query = "[" + min + " TO " + max + "]";
+          // query parameters
+          var min = start_date ? start_date : "*";
+          var max = end_date ? end_date : "*";
+          var query = "[" + min + " TO " + max + "]";
 
-        self.clear(); // clear existing filter query for date range
-        if (self.add(query)) {
-          self.doRequest();
-        }
-      });
+          self.clear(); // clear existing filter query for date range
+          if (self.add(query)) {
+            self.doRequest();
+          }
+        });
     },
   });
 })(jQuery);
